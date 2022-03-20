2b3ccd2f850b5c032bbd89d6f6827b223b3bb272
Eduardo <57041009+eduespuch@users.noreply.github.com>
2022-03-20 18:50:14 +0100

    Processing v0.5
    Added sensibility factors and implemented more cases of changing states

--- processing/src/processing.cpp
+++ processing/src/processing.cpp
@@ -10,8 +10,15 @@ class Procesing {
 	//The node handle we'll be using
 	ros::NodeHandle nh_;
 	ros::Publisher chatter_pub;
-	ros::Subscriber laserSub; 
-	int count;
+	ros::Subscriber laserSub;
+	double const frontUmbral=0.65;//vigilar la sensibilidad con la que hace el giro
+	double const shortLateralUmbral=0.3;//vigilar la sensibilidad con la que se le permite acercarse a la pared
+	double const largeLateralUmbral=2.8;//vigilar la sensibilidad con la que se le permite alejarse de la pared
+	double const angleCurveUmbral=0.349066; //20º sensibilidad de curva
+	double const angleTurnUmbral=0.610865;//35º sensibilidad de giro
+	double const detectableRange=0.14;//Para gestionar error que se genera con el lidar al moverse el robot
+	double const increaseFactor=1.2;//Factor para forzar a a giros/curvas mas intensas
+
 
 
 	public:
@@ -22,20 +29,24 @@ class Procesing {
 
 		chatter_pub = nh.advertise<std_msgs::String>("info",1000);
 		nh_ = nh;
-		count=0;
 	};
 
 	// Procesa los datos de láser
 	void commandCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
-		
+
+		//parametros necesarios
+
 		std_msgs::String msgInfo;
 	    std::stringstream ss;
-
-		//Obtiene el angulo de interseccion a la pared de la izquierda
-
 		int firstAngle=50;//punto mas cercano al frente
 		int secAngle=51;//ultimo punto posible
-		bool first=false;
+		bool first=false;//indica si se ha encontrado un punto al menos en el area de deteccion a la izquierda
+		double minDistanceFrontal=msg->range_max;
+		double minDistanceLateral=msg->range_max;
+		int relAngleFrontal=360;
+		int relAngleLateral=360;
+
+		//Obtiene el angulo de interseccion a la pared de la izquierda
 
 		for(int i= 50;i<=100;i++){
 			if(!first&&msg->ranges[i]<=3.5){
@@ -46,6 +57,10 @@ class Procesing {
 			if(first&&msg->ranges[i]<=3.5){
 				secAngle=i;
 			}
+			if(detectableRange<msg->ranges[i] && msg->ranges[i]<minDistanceLateral){
+				minDistanceLateral=msg->ranges[i];
+				relAngleLateral=i;
+			}
 		}
 
 		double o=(sin((msg->range_max-secAngle)*M_PI/180)*msg->ranges[secAngle]-sin((msg->range_max-firstAngle)*M_PI/180)*msg->ranges[firstAngle]); 
@@ -55,50 +70,77 @@ class Procesing {
 
 		//Extraer los datos de interes
 
-		double minDistance=msg->range_max;
-		double detectableRange=0.18;
-		double frontUmbral=0.25;
-		double lateralUmbral=0.3;
-		double angleCurveUmbral=0.349066; //20º
-		int relAngle=360;
-
 		for(int i=0;i<=25;i++){
-			if(detectableRange<msg->ranges[i] && msg->ranges[i]<minDistance){
-				minDistance=msg->ranges[i];
-				relAngle=i;
+			if(detectableRange<msg->ranges[i] && msg->ranges[i]<minDistanceFrontal){
+				minDistanceFrontal=msg->ranges[i];
+				relAngleFrontal=i;
 			}
 		}
 		for(int i=335;i<=358;i++){
-			if(detectableRange<msg->ranges[i] && msg->ranges[i]<minDistance){
-				minDistance=msg->ranges[i];
-				relAngle=i;
+			if(detectableRange<msg->ranges[i] && msg->ranges[i]<minDistanceFrontal){
+				minDistanceFrontal=msg->ranges[i];
+				relAngleFrontal=i;
 			}
 		}
 
 		if(!first){
 			ROS_INFO_STREAM("Posible ruta a la izquierda");
-			wallAngle=M_PI;			
+			wallAngle=M_PI/2;			
 		}
 
 
 		//Retocar condiciones de estado
 
-		if(minDistance<frontUmbral){//obstruccion delante, tiene que girar
+		if(wallAngle>angleTurnUmbral||wallAngle<(-1*angleTurnUmbral)){//umbral de giro alto
 			ss<< "3";
-		}else if(wallAngle>angleCurveUmbral||wallAngle<(-1*angleCurveUmbral)){
+			ROS_INFO_STREAM("Caso giro de umbral alto. Realiza giro de: "<<wallAngle*180/M_PI<<"º");
+
+		}else if(minDistanceFrontal<frontUmbral){//obstruccion delante, tiene que girar pero angulo de giro escaso
+			ss<< "3";
+			wallAngle=wallAngle>0?M_PI/2:-M_PI/2;//metodo temporal
+			ROS_INFO_STREAM("Caso giro de colision. Realiza giro de: "<<wallAngle*180/M_PI<<"º");
+
+
+			//Implementar este pseudocodigo mirando los parametros necesarios
+			//modificar angulo segun condiciones de la situacion
+			//si delante y a la izquierda parece estar bloqueado, ir a la derecha (minDistanceLateral<ranges[270], por ejemplo)
+				//wallAngle=M_PI/2*(-1);
+			//si no, a la izquierda se puede realizar un giro de 90º
+				//wallAngle=M_PI/2;
+
+
+		}else if(wallAngle>angleCurveUmbral||wallAngle<(-1*angleCurveUmbral)){//umbral curva leve
 			ss<<"2";
+			ROS_INFO_STREAM("Caso curva de umbral leve. Realiza giro de: "<<wallAngle*180/M_PI<<"º");
+
+			
+		}else if(minDistanceLateral<shortLateralUmbral){//acercarse a la pared, curva hacia fuera (angulo)
+				//se aleja pared, curva hacia ella (angulo*-1)
+			ss<<"2";
+			wallAngle=wallAngle*increaseFactor;
+			ROS_INFO_STREAM("Caso curva de acercamiento de pared. Realiza giro de: "<<wallAngle*180/M_PI<<"º");
+
+			
+		}else if(minDistanceLateral>largeLateralUmbral){//acercarse a la pared, curva hacia fuera (angulo)
+				//se aleja pared, curva hacia ella (angulo*-1)
+			ss<<"2";
+
+			wallAngle=-wallAngle*increaseFactor;
+			ROS_INFO_STREAM("Caso curva de alejamiento de pared. Realiza giro de: "<<wallAngle*180/M_PI<<"º");
+
+			
 		}else{
+			ROS_INFO_STREAM("Caso recto. No realiza giro.");
 			ss<<"1";
 		}
 
-		ss<<":"<<wallAngle<<":"<<minDistance;
+		ss<<":"<<wallAngle<<":"<<minDistanceFrontal;
 
   	    msgInfo.data = ss.str();
 
 		chatter_pub.publish(msgInfo);
 
 		ROS_INFO("%s", msgInfo.data.c_str());
-		ROS_INFO_STREAM("Sensores empleados: ("<<firstAngle<<","<<secAngle<<","<<relAngle<<")\n");
 
 
 	};
