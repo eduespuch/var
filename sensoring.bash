332ac8a0e2e7fa4f8552be4ea3a4a7000fdb23a8
Eduardo <57041009+eduespuch@users.noreply.github.com>
2022-04-10 12:06:57 +0200

    Updated additional scripts for P2

---
+++ sensoring.bash
@@ -0,0 +1,12 @@
+catkin_make
+
+#Por si se pone chulo
+export TURTLEBOT3_MODEL=waffle
+export TURTLEBOT3_MODEL=waffle
+export TURTLEBOT3_MODEL=waffle
+
+source devel/setup.bash
+source devel/setup.bash
+source devel/setup.bash
+
+rosrun sensoring sensoring
