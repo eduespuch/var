cd32639183a813b31e9d22e99c618ffcd83f8cf3
JoseLuis9399 <79456853+JoseLuis9399@users.noreply.github.com>
2022-04-15 13:20:03 +0200

    V0.2
    Añadido cómputo de descriptores FPFH.

--- sensoring/src/sensoring.cpp
+++ sensoring/src/sensoring.cpp
@@ -6,6 +6,8 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/fpfh.h>
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
 
@@ -22,6 +24,34 @@ void simpleVis ()
     
 }
 
+void FPFH(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud){
+
+	pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);//Dataset de normales respecto a la nube de puntos.
+	pcl::PointCloud<pcl::FPFHSignature33>::Ptr descriptors (new pcl::PointCloud<pcl::FPFHSignature33> ());//Dataset de descriptores respecto a la nube de puntos.
+
+	pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normalEstimation;//Esto se usa para estimar las normales.
+	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+
+	pcl::FPFHEstimation<pcl::PointXYZRGB, pcl::Normal, pcl::FPFHSignature33> fpfh;//Esto se usa para calcular el descriptor de histograma.
+
+
+	normalEstimation.setInputCloud(cloud);//Prepara la nube de puntos capturados para extraer sus rectas normales.
+	fpfh.setInputCloud(cloud);//Prepara la nube de puntos para obtener sus descriptores, en base a la propia nube de puntos y al dataset de normales.
+	
+	normalEstimation.setSearchMethod(kdtree);//La búsqueda la realizará fijándose en los n vecinos que encuentre en una esfera de radio r.
+	normalEstimation.setRadiusSearch(0.03);//Da valor al radio r.
+	normalEstimation.compute(*normals);//Calcula las normales de la nube de puntos.
+
+	fpfh.setInputNormals(normals);//Una vez calculadas las normales, las podemos usar para obtener el histograma.
+  
+	fpfh.setSearchMethod(kdtree);
+  	fpfh.setRadiusSearch(0.05);//Establece el radio de búsqueda (en metros) para los vecinos,el radio usado aquí debe ser mayor que el usado en el cálculo de las normales.
+	fpfh.compute(*descriptors);
+
+	cout<<"Se han calculado: "<<descriptors->points.size()<<" descriptores."<<endl;
+
+}
+
 
 void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
 {
@@ -29,6 +59,7 @@ void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
 
+	
 	cout << "Puntos capturados: " << cloud->size() << endl;
 
 	pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
@@ -39,7 +70,8 @@ void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
 	cout << "Puntos tras VG: " << cloud_filtered->size() << endl;
 
 	visu_pc = cloud_filtered;
-    
+
+	FPFH(cloud_filtered);//Método 1 de cálculo de los descriptores.
 
 }
 
@@ -57,3 +89,5 @@ int main(int argc, char** argv)
   }
 
 }
+
+
